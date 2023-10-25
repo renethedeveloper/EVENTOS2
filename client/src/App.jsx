bd20b31b98d8ffc0cb5da423368b083aaa266e8e
Rene Rodriguez <afuereno@gmail.com>
2023-10-25 15:30:26 -0700

    again

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,8 @@ import './App.css'
 import { useState } from 'react'
 import EventForm from './components/EventForm'
 import EventList from './components/EventList'
-import ViewEmployee from './components/viewEmployee.jsx';
-import EmployeeForm from './components/EmployeeForm';
+// import ViewEmployee from './components/viewEmployee.jsx';
+
 
 
 
@@ -17,8 +17,8 @@ function App() {
       <h1>My Events</h1>
       <EventForm  setEvents={setEvents} />
       <EventList events={events} setEvents={setEvents} />
-      <ViewEmployee />
-      <EmployeeForm />
+      {/* <ViewEmployee />
+      <EmployeeForm /> */}
     </>
   )
 }
